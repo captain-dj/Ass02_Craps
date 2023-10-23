cf088e5023500ed36061be2ba683376fd6282b24
captain-dj <whardig10@gmail.com>
2023-10-23 16:51:53 -0400

    1.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        while (true) {
+            System.out.println("Press Enter to roll the dice...");
+            scanner.nextLine();
+
+            int die1 = random.nextInt(6) + 1;
+            int die2 = random.nextInt(6) + 1;
+            int sum = die1 + die2;
+
+            System.out.println("Roll: " + die1 + " + " + die2 + " = " + sum);
+
+            if (sum == 2 || sum == 3 || sum == 12) {
+                System.out.println("Craps! You lost.");
+            } else if (sum == 7 || sum == 11) {
+                System.out.println("Natural! You won.");
+            } else {
+                System.out.println("Point is " + sum);
+                boolean continueRolling = true;
+
+                while (continueRolling) {
+                    System.out.println("Press Enter to roll the dice again...");
+                    scanner.nextLine();
+
+                    die1 = random.nextInt(6) + 1;
+                    die2 = random.nextInt(6) + 1;
+                    int newSum = die1 + die2;
+
+                    System.out.println("Roll: " + die1 + " + " + die2 + " = " + newSum);
+
+                    if (newSum == sum) {
+                        System.out.println("Made point and won!");
+                        continueRolling = false;
+                    } else if (newSum == 7) {
+                        System.out.println("Got a seven and lost.");
+                        continueRolling = false;
+                    } else {
+                        System.out.println("Trying for point...");
+                    }
+                }
+            }
+
+            System.out.println("Do you want to play again? (Y/N)");
+            String choice = scanner.nextLine().toLowerCase();
+            if (!choice.equals("Y")) {
+                break;
+            }
+        }
+
+        System.out.println("Thanks play again another time!");
     }
 }
